667b092d2aa30e28a927bdd30ab8430cf85fe18b
Luke Taylor <luke@deentaylor.com>
2020-09-06 01:09:35 -0400

    Copy supporting files in build script

--- build/index.js
+++ build/index.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import glob from 'globby';
 import Handlebars from 'handlebars';
 
 import { getDateComponent, formatDateRange } from './dates.js';
@@ -18,3 +19,12 @@ const template = await fs.readFile(path.join(srcDir, 'template.hbs'), 'utf-8');
 const templateFunc = Handlebars.compile(template);
 const output = templateFunc(data);
 await fs.writeFile(path.join(outDir, 'index.html'), output, 'utf-8');
+
+// Copy supporting files
+await glob('*.{css,svg}', { cwd: srcDir, onlyFiles: true })
+  .then((files) => Promise.all(
+    files.map((f) => fs.copyFile(
+      path.join(srcDir, f),
+      path.join(outDir, f),
+    )),
+  ));
