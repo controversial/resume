d20741f84178ca53db6de7fd1017e8a892b58edf
Luke Taylor <luke@deentaylor.com>
2020-09-07 02:28:07 -0400

    Tests for content

---
+++ src/content.test.js
@@ -0,0 +1,45 @@
+/* global test, expect */
+
+import content from './content.js';
+
+test('All content keys are present', () => {
+  const expectedKeys = ['about', 'experience', 'work', 'education', 'skills', 'contact'];
+  expect(Object.keys(content).sort()).toEqual(expectedKeys.sort());
+});
+
+test('startDates come before endDates', () => {
+  // Get all date ranges from content (including each range of multi-ranges)
+  const dateRanges = [
+    ...content.experience,
+    ...content.work,
+    ...content.education,
+  ].flatMap(({ startDate, endDate }) => {
+    if (Array.isArray(startDate)) {
+      expect(endDate || []).toBeInstanceOf(Array);
+      if (endDate) expect(endDate.length).toBe(startDate.length);
+      return startDate.map((_, i) => ({ startDate: startDate[i], endDate: endDate[i] }));
+    }
+    return { startDate, endDate };
+  });
+
+  dateRanges.forEach(({ startDate, endDate }) => {
+    if (startDate && endDate) {
+      expect(startDate).toBeInstanceOf(Date);
+      expect(endDate).toBeInstanceOf(Date);
+      expect(startDate.getTime()).toBeLessThanOrEqual(endDate.getTime());
+    }
+  });
+});
+
+test('Longform content ends with a period', () => {
+  const longContent = [
+    ...content.about.map((a) => a.description),
+    ...content.experience.map((e) => e.tasks),
+    ...content.work.map((w) => w.description),
+    ...content.skills.map((s) => s.description),
+  ].flat().filter((item) => !!item);
+
+  longContent.forEach((text) => {
+    expect(text.trim().endsWith('.')).toBe(true);
+  });
+});
