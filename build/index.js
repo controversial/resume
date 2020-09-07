c319b388d6289e4d16ac09bad1bfb3ae4a6ca278
Luke Taylor <luke@deentaylor.com>
2020-09-06 21:20:00 -0400

    Improve build concurrency

--- build/index.js
+++ build/index.js
@@ -1,11 +1,10 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 import glob from 'globby';
+
 import Handlebars from 'handlebars';
 
 import { getDateComponent, formatDateRange } from './dates.js';
-
-import data from '../src/content.js';
 
 const srcDir = path.resolve(process.cwd(), 'src');
 const outDir = path.resolve(process.cwd(), 'out');
@@ -27,17 +26,28 @@ Handlebars.registerHelper('displayLink', (value, type) => {
   return new Handlebars.SafeString(out);
 });
 
-// Compile HTML
-const template = await fs.readFile(path.join(srcDir, 'template.hbs'), 'utf-8');
-const templateFunc = Handlebars.compile(template);
-const output = templateFunc(data);
-await fs.writeFile(path.join(outDir, 'index.html'), output, 'utf-8');
+// 1) Generate output
+Promise.all([
+  // b) Compile HTML
+  (async () => {
+    const [content, templateFunc] = await Promise.all([
+      import('../src/content.js').then((module) => module.default),
+      fs.readFile(path.join(srcDir, 'template.hbs'), 'utf-8')
+        .then((template) => Handlebars.compile(template)),
+    ]);
+    return templateFunc(content);
+  })(),
+  // a) Get supporting files
+  glob('*.{css,svg}', { cwd: srcDir, onlyFiles: true }),
+])
 
-// Copy supporting files
-await glob('*.{css,svg}', { cwd: srcDir, onlyFiles: true })
-  .then((files) => Promise.all(
-    files.map((f) => fs.copyFile(
+// 2) Write output to disk
+  .then(([html, filesToCopy]) => Promise.all([
+    // a) Write HTML
+    fs.writeFile(path.join(outDir, 'index.html'), html, 'utf-8'),
+    // b) Write supporting files
+    ...filesToCopy.map((f) => fs.copyFile(
       path.join(srcDir, f),
       path.join(outDir, f),
     )),
-  ));
+  ]));
