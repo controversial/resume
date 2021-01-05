5c7efc79e9c218ae13bbf3b7d903561c6362ccea
Luke Taylor <luke@deentaylor.com>
2021-01-04 21:20:59 -0500

    Rename 'out' to 'dist'

--- build/paths.js
+++ build/paths.js
@@ -1,4 +1,4 @@
 import path from 'path';
 
 export const srcDir = path.resolve(process.cwd(), 'src');
-export const outDir = path.resolve(process.cwd(), 'out');
+export const outDir = path.resolve(process.cwd(), 'dist');
